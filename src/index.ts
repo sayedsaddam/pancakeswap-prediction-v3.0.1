8ea6de1590721ddae51c28d07f0343e13a66fe3b
Sayed Saddam Hussain <48250591+sayedsaddam@users.noreply.github.com>
2022-07-28 17:34:00 +0500

    updated index.ts, changed the log message.

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const GLOBAL_CONFIG = {
 };
 
 clear();
-console.log(green("PancakeSwapPredictionV3.0.1 Bot winner."));
+console.log(green("PancakeSwapPredictionV3.0.1 Bot winner"));
 
 if (!GLOBAL_CONFIG.PRIVATE_KEY) {
   console.log(
